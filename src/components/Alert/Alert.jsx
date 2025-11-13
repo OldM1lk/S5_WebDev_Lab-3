a431d558228db9af2ff9575bd148cf6f92196fda
OldM1lk <mrlaimwin@gmail.com>
2025-11-13 16:36:29 +0700

    Refactor alert

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -1,19 +1,17 @@
 import "./Alert.css"
 
-export const Alert = ({onCancelClick, onConfirmClick}) => {
-    const onAlertClick = e => {
-        if (e.target === e.currentTarget) {
-            onCancelClick()
-        }
-    }
-
+export const Alert = ({onConfirm, onCancel}) => {
     return (
-        <div className="alert" onClick={onAlertClick}>
+        <div className="alert">
             <div className="alert__content">
                 <p className="alert__text">Delete this task?</p>
                 <div className="alert__buttons">
-                    <button className="button-confirm" onClick={onConfirmClick}>Yes</button>
-                    <button className="button-cancel" onClick={onCancelClick}>No</button>
+                    <button className="button-confirm" onClick={onConfirm}>
+                        Yes
+                    </button>
+                    <button className="button-cancel" onClick={onCancel}>
+                        No
+                    </button>
                 </div>
             </div>
         </div>
