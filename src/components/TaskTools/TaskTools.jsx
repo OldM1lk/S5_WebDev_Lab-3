cd44e81a00c370b2acf9f7d9544540df35bebf8a
OldM1lk <mrlaimwin@gmail.com>
2025-11-13 16:10:44 +0700

    Add props in task tools component

--- src/components/TaskTools/TaskTools.jsx
+++ src/components/TaskTools/TaskTools.jsx
@@ -1,18 +1,23 @@
 import "./TaskTools.css"
-import {useState} from "react";
 
-export const TaskTools = () => {
-
-
+export const TaskTools = ({task, onShare, onEdit}) => {
     return (
         <div className="task__tools">
-            <button className="button-share" onClick={() => {
-            }}>
+            <button
+                className="button-share"
+                onClick={(e) => {
+                    e.stopPropagation()
+                    onShare(task)
+                }}>
                 <img src="/svg/share-icon.svg" alt="Share Icon"/>
             </button>
             <button className="button-info">i</button>
-            <button className="button-edit" onClick={() => {
-            }}>
+            <button
+                className="button-edit"
+                onClick={(e) => {
+                    e.stopPropagation()
+                    onEdit(task)
+                }}>
                 <img src="/svg/pencil-icon.svg" alt="Pencil Icon"/>
             </button>
         </div>
