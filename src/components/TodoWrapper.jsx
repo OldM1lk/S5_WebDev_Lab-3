d7e737323cb802c88fb1fe9d94bd3e915576458c
OldM1lk <mrlaimwin@gmail.com>
2025-12-21 06:12:25 +0700

    Add toggle pin handler and some refactor

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,15 +1,16 @@
-import {useState} from "react";
-import {Form} from "./Form/Form.jsx";
-import {Tasks} from "./Tasks/Tasks.jsx";
-import {Alert} from "./Alert/Alert.jsx";
-import {EditWindow} from "./EditWindow/EditWindow.jsx";
-import {ShareBox} from "./ShareBox/ShareBox.jsx";
-import {useDispatch, useSelector} from "react-redux";
-import {addTodo, deleteTodo, editTodo} from "../todoSlice.js";
+import {useState} from "react"
+import {Form} from "./Form/Form.jsx"
+import {Tasks} from "./Tasks/Tasks.jsx"
+import {Alert} from "./Alert/Alert.jsx"
+import {EditWindow} from "./EditWindow/EditWindow.jsx"
+import {ShareBox} from "./ShareBox/ShareBox.jsx"
+import {useDispatch, useSelector} from "react-redux"
+import {addTodo, deleteTodo, editTodo, togglePin} from "../todoSlice.js"
 
 export const TodoWrapper = () => {
-    const tasks = useSelector(state => state.todos.items);
-    const dispatch = useDispatch();
+    const tasks = useSelector(state => state.todos.items)
+    const dispatch = useDispatch()
+
     const [showAlert, setShowAlert] = useState(false)
     const [showEdit, setShowEdit] = useState(false)
     const [showShare, setShowShare] = useState(false)
@@ -22,32 +23,35 @@ export const TodoWrapper = () => {
         dispatch(deleteTodo(id))
     }
     const editTask = (id, newTitle, newDescription) => {
-        if (!newTitle.trim()) return;
+        if (!newTitle.trim()) return
         dispatch(editTodo({id, title: newTitle, description: newDescription}))
     }
 
     const openDelete = (task) => {
-        setSelectedTask(task);
-        setShowAlert(true);
+        setSelectedTask(task)
+        setShowAlert(true)
     }
     const openEdit = (task) => {
-        setSelectedTask(task);
-        setShowEdit(true);
+        setSelectedTask(task)
+        setShowEdit(true)
     }
     const openShare = () => {
-        setShowShare(true);
+        setShowShare(true)
     }
 
     const handleDeleteConfirm = () => {
         if (selectedTask) {
-            deleteTask(selectedTask.id);
+            deleteTask(selectedTask.id)
         }
-        setShowAlert(false);
-        setSelectedTask(null);
+        setShowAlert(false)
+        setSelectedTask(null)
     }
     const handleDeleteCancel = () => {
-        setShowAlert(false);
-        setSelectedTask(null);
+        setShowAlert(false)
+        setSelectedTask(null)
+    }
+    const handleTogglePin = (task) => {
+        dispatch(togglePin(task.id))
     }
 
     return (
@@ -58,6 +62,7 @@ export const TodoWrapper = () => {
                 onDelete={openDelete}
                 onEdit={openEdit}
                 onShare={openShare}
+                onTogglePin={handleTogglePin}
             />
 
             {showAlert && (
@@ -75,8 +80,8 @@ export const TodoWrapper = () => {
                         setShowEdit(false)
                     }}
                     onCancel={() => {
-                        setShowEdit(false);
-                        setSelectedTask(null);
+                        setShowEdit(false)
+                        setSelectedTask(null)
                     }}
                 />
             )}
